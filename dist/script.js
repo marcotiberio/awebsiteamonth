1308df0db6a3271aaceee217ebe6c2247dfdff58
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2019-08-01 17:25:52 +0200

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,15 @@
+// function myFunction() {
+//   document.getElementById("mainImage").style.width = "100%";
+//   document.getElementById("mainImage").style.top = "0";
+// }
+
+
+// function function000() {
+//   document.getElementById("mainImage000").style.width = "100%";
+//   document.getElementById("mainImage000").style.top = "0";
+// }
+//
+// var myFullpage = new fullpage('#fullpage', {
+//     verticalCentered: true,
+//     sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE']
+// });
