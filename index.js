5994cdb5a34bfa3379301de7e89aefe6aa24fd92
Marco Tiberio <tiberiomrc@gmail.com>
2019-10-30 20:10:36 +0100

    edits home + clock

---
+++ index.js
@@ -0,0 +1,21 @@
+// CLOCK
+
+function clock() { // We create a new Date object and assign it to a variable called "time".
+var time = new Date(),
+
+    // Access the "getHours" method on the Date object with the dot accessor.
+    hours = time.getHours(),
+    
+    // Access the "getMinutes" method with the dot accessor.
+    minutes = time.getMinutes();
+
+document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(minutes);
+
+  function harold(standIn) {
+    if (standIn < 10) {
+      standIn = '0' + standIn;
+    }
+    return standIn;
+  }
+}
+setInterval(clock, 1000);
