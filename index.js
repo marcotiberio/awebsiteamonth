63dbb88a87547121808be245fbbce53bfa03d971
Marco Tiberio <tiberiomrc@gmail.com>
2019-11-27 23:09:36 +0100

    update

--- index.js
+++ index.js
@@ -19,10 +19,3 @@ document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(
   }
 }
 setInterval(clock, 1000);
-
-
-// LOADER
-
-$(document).ready(function() {
-  $("#loader").fadeOut();
-});
